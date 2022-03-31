111d515bffb3df43fa3d9b48684084de221c0597
Denis <omelched@gmail.com>
2022-03-31 19:11:53 +0300

    :bug: fix error when ContentType already interested

--- djangoRLS/admin/__init__.py
+++ djangoRLS/admin/__init__.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.admin.sites import AlreadyRegistered
 
 from djangoRLS.admin.RLSManagedModelAdmin import RLSManagedModelAdmin
 from djangoRLS.models import RLSPermission
 from djangoRLS.admin.models import RLSPermissionAdmin
 
-
 admin.site.register(RLSPermission, RLSPermissionAdmin)
-admin.site.register(ContentType)
+
+try:
+    admin.site.register(ContentType)
+except AlreadyRegistered:
+    pass
